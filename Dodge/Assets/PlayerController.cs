16f155741d3a671b59dc3a5805c3a57de9d7f8ef
보형 <bohyeong99@gmail.com>
2023-04-19 12:45:09 +0900

    0419
    274p

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -4,45 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody; 
+    private Rigidbody playerRigidbody;
     // 이동에 사용할 리지드바디 컴포넌트
     public float speed = 8f; 
     //이동 속력
-    // Start is called before the first frame update
     void Start()
     {
-
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
+        //수평축과 수직축의 입력값을 감지하여 저장
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
 
-            playerRigidbody.AddForce(0f, 0f, speed); 
-            //위쪽 방향키 입력이 감지된 경우 z방향 힘 주기
-        }
+        //실제 이동 속도를 입력값과 이동 속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
 
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-
-            playerRigidbody.AddForce(0f, 0f, -speed); 
-            //아래쪽 방향키 입력이 감지된 경우 -z 방향 힘 주기
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-
-            playerRigidbody.AddForce(speed, 0f, 0f); 
-            //오른쪽 방향키 입력이 감지된 경우 x 방향 힘 주기
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-
-            playerRigidbody.AddForce(-speed, 0f, 0f); 
-            //왼쪽 방향키 입력이 감지된 경우 -x 방향 힘 주기
-        }
+        //Vector3 속도를 (xSpeed, 0, zSpeed)로 생성
+        Vector3 newvelocity = new Vector3(xSpeed, 0f, zSpeed);
+        //리지드바디의 속도에 newVelocity 할당
+        playerRigidbody.velocity = newvelocity;
+        
     }
   public void die()
     {
